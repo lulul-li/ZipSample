36464bcb35e528562bfe25225b4abb5ada92797f
lulu.li29 <s987337613@gmail.com>
2018-12-01 10:34:46 +0800

    add Medical test

--- ZipSample/test/UnitTest1.cs
+++ ZipSample/test/UnitTest1.cs
@@ -1,34 +1,85 @@
-﻿using ExpectedObjects;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+using NUnit.Framework;
+using Assert = Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
 
 namespace ZipSample.test
 {
-    [TestClass]
-    public class ZipTests
+    [TestFixture]
+    public class UnitTest1
     {
-        [TestMethod]
-        public void pair_3_girls_and_5_boys()
+        [TestCase(106, "2", true)]
+        [TestCase(106, "11", true)]
+        [TestCase(106, "0", false)]
+        [TestCase(106, "12", false)]
+        [TestCase(106, "99", true)]
+        [TestCase(107, "2a", true)]
+        [TestCase(107, "2b", true)]
+        [TestCase(107, "14", true)]
+        [TestCase(107, "1", true)]
+        [TestCase(107, "2", false)]
+        [TestCase(107, "12", false)]
+        [TestCase(107, "99", true)]
+        [TestCase(107, "abc", false)]
+        public void valid_list(int year, string content, bool expected)
         {
-            var girls = Repository.Get3Girls();
-            var keys = Repository.Get5Keys();
+            var medical = new Medical(year, content);
+            Assert.AreEqual(expected, medical.IsValid());
+        }
+    }
 
-            var girlAndBoyPairs = MyZip(girls, keys).ToList();
-            var expected = new List<Tuple<string, string>>
-            {
-                Tuple.Create("Jean", "Joey"),
-                Tuple.Create("Mary", "Frank"),
-                Tuple.Create("Karen", "Bob"),
-            };
+    public class Medical
+    {
+        public int Year { get; }
+        public string Content { get; }
 
-            expected.ToExpectedObject().ShouldEqual(girlAndBoyPairs);
+        public Medical(int year, string content)
+        {
+            Year = year;
+            Content = content;
         }
 
-        private IEnumerable<Tuple<string, string>> MyZip(IEnumerable<Girl> girls, IEnumerable<Key> keys)
+        public bool IsValid()
         {
-            throw new NotImplementedException();
+            if (Year != 106 && Year != 107)
+            {
+                return false;
+            }
+
+            if (Content=="99")
+            {
+                return true;
+            }
+
+            //new Dictionary<int,Func<string,bool>>
+            //{
+            //    { 106,(c)=> c=="2a"||c=="2b"},
+            //}
+            if (Year==106)
+            {
+                var content = Convert.ToInt32(Content);
+                if (content>=1  && content<=11)
+                {
+                    return true;
+                }
+            }
+            if (Year == 107)
+            {
+                if (Content=="2a"||Content=="2b")
+                {
+                    return true;
+                }
+                if (Int32.TryParse(Content, out var content))
+                {
+                    if ((content >= 1 || content <= 14) && (content != 2 && content != 12))
+                    {
+                        return true;
+                    }
+                }
+
+               
+            }
+            return false;
         }
     }
 }
